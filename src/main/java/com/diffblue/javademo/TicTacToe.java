e1e75a95d3339652cc485ae8aa2d1c4d2bcbdf44
James wilson <james.wilson@diffblue.com>
2017-11-24 12:37:01 +0000

    Refactored

--- src/main/java/com/diffblue/javademo/TicTacToe.java
+++ src/main/java/com/diffblue/javademo/TicTacToe.java
@@ -15,7 +15,7 @@ public class TicTacToe {
    *   1: player X has won
    *   2: player O has won
    */
-  public byte checkTicTacToePosition(byte[] a) {
+    public byte checkTicTacToePosition(byte[] a) {
     // First check number of cells
     if (a.length != 9) {
       return -1;
@@ -41,40 +41,26 @@ public class TicTacToe {
     // Otherwise we know we have a valid board state
 
     // Check if a row and column has won
-    for (byte i = 0; i < 3; i++) {
-      if (a[i] == a[3 + i] && a[i] == a[6 + i]) {
-        if (a[i] == 1) {
-          return 1;
-        } else if (a[i] == 2) {
-          return 2;
+    for (int i = 0; i < 3; i++) {
+      if (a[i] != 0) {
+        if (a[i] == a[3 + i] && a[i] == a[6 + i]) {
+	  return a[i];
         }
-      }
 
-      if (a[3 * i] == a[3 * i + 1] && a[3 * i] == a[3 * i + 2]) {
-        if (a[3 * i] == 1) {
-          return 1;
-        } else if (a[3 * i] == 2) {
-          return 2;
+        if (a[3 * i] == a[3 * i + 1] && a[3 * i] == a[3 * i + 2]) {
+	  return a[3 * i];
         }
       }
     }
 
     // Check if the top-left bottom-right diagonal has won
-    if (a[0] == a[4] && a[0] == a[8]) {
-      if (a[0] == 1) {
-        return 1;
-      } else if (a[0] == 2) {
-        return 2;
-      }
+    if (a[0] == a[4] && a[0] == a[8] && a[0] != 0) {
+	return a[0];
     }
 
     // Check if the top-right bottom-left diagonal has won
-    if (a[2] == a[4] && a[2] == a[6]) {
-      if (a[2] == 1) {
-        return 1;
-      } else if (a[2] == 2) {
-        return 2;
-      }
+    if (a[2] == a[4] && a[2] == a[6]&& a[2] != 0) {
+	return a[2];
     }
 
     // Neither player has won yet
