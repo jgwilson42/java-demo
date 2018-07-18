ac9f621bc014b1e731f2a597314d066d221e0c67
James Wilson <james.wilson@diffblue.com>
2018-07-18 14:07:29 +0200

    Signature for functions

---
+++ src/main/java/com/diffblue/javademo/Calculator.java
@@ -0,0 +1,67 @@
+package com.diffblue.javademo;
+
+public class Calculator {
+
+  private int mem1;
+  private int mem2;
+  private int total;
+
+  /**
+   * foo.
+   */
+  public Calculator(int mem1, int mem2, int total) {
+    this.mem1 = mem1;
+    this.mem2 = mem2;
+    this.total = total;
+  }
+
+  /**
+   * foo.
+   */
+  public Calculator(int mem1, int mem2) {
+    this.mem1 = mem1;
+    this.mem2 = mem2;
+    this.total = 0;
+  }
+
+  /**
+   * foo.
+   */
+  public Calculator() {
+    mem1 = 0;
+    mem2 = 0;
+    total = 0;
+  }
+
+  /**
+   * foo.
+   */
+  public int add(int num) {
+    // TODO: implement this
+    return total;
+  }
+
+  /**
+   * foo.
+   */
+  public int add(int num1, int num2) {
+    // TODO: implement this
+    return total;
+  }
+
+  /**
+   * foo.
+   */
+  public int div(int num) {
+    // TODO: implement this
+    return total;
+  }
+
+  /**
+   * foo.
+   */
+  public int div(int num1, int num2) {
+    // TODO: implement this
+    return total;
+  }
+}
